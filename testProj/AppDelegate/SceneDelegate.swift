e198c8c399797644d432658c36969ad63142918d
Denis Kravets <d.kravets@napolke.ru>
2024-03-26 19:58:34 +0400

    - add(testProj): создал проект

---
+++ testProj/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  SceneDelegate.swift
+//  testProj
+//
+//  Created by Denis Kravets on 26.03.2024.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    // MARK: Properties
+    
+    var window: UIWindow?
+    
+    // MARK: Methods
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
